8b5f5143b4c43c65104c97966eb87c7238ecb77c
Dumitru Cristian <78100242+CrisAxe@users.noreply.github.com>
2026-04-16 21:57:22 +0100

    add:add new feet

--- reservation.js
+++ reservation.js
@@ -1,5 +1,26 @@
-let reservation = [];
+const form = document.querySelector(".form-reserva");
 
-function addReservation(name, date) {
-    reservation.push({ name: name, date: date });
-}
+form.addEventListener("submit", function(e) {
+    e.preventDefault(); 
+    const nome = document.querySelector("input[name='nome']").value
+    const email = document.querySelector("input[name='email']").value;
+    const telefone = document.querySelector("input[name='telefone']").value;
+    const data = document.querySelector("input[name='data']").value;
+    const hora = document.querySelector("input[name='hora']").value;
+    const pessoas = document.querySelector("input[name='pessoas']").value;
+    const obs = document.querySelector("textarea[name='obs']").value;
+
+    const dados = {
+        nome: nome,
+        email: email,
+        telefone: telefone,
+        data: data,
+        hora: hora,
+        pessoas: pessoas,
+        obs: obs
+    };
+
+    localStorage.setItem("reserva", JSON.stringify(dados));
+
+    alert("Reserva guardada localmente!");
+  });

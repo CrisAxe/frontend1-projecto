69198ac6f80ab5520859f833559137960245f011
Dumitru Cristian <78100242+CrisAxe@users.noreply.github.com>
2026-04-30 21:37:47 +0100

    fix:fix card

--- js/pituxa-card.js
+++ js/pituxa-card.js
@@ -6,13 +6,18 @@ class PituxaCard extends HTMLElement {
         shadow.innerHTML = `
             <style>
                 .card {
+                    width: 600px;
                     padding: 20px;
+                    max-width: 90%;
                     background: rgba(0, 0, 0, 0.7);
                     border-radius: 12px;
-                    border: 1px solid rgba(255, 255, 255, 0.1);
+                    border: 1px solid rgba(255, 255, 255, 0.69);
                     color: #f8f8f8;
                     font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", sans-serif;
-                    box-shadow: 0 10px 30px rgba(0, 0, 0, 0.4);
+                    box-shadow: 0 10px 30px rgba(255, 255, 255, 0.4);
+                    margin: 30px auto:
+                    display: block;
+                    text-align: center;
                 }
                 .title {
                     font-weight: 600;
@@ -36,3 +41,4 @@ class PituxaCard extends HTMLElement {
 }
 
 customElements.define("pituxa-card", PituxaCard);
+
